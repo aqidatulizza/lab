dc389d41c9a864c97ace2a051c0a1c6ef869da39
izza <aqiza18@gmail.com>
2022-03-07 12:38:38 +0700

    login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login Lab</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poppins" />
+    
+    </head>
+<body>
+    <div class="container">
+        <div class="login">
+            <form action="">
+                <div class="tang">
+                    <img src="images/tang.png" alt="">
+                </div>
+                <h1>Login</h1>
+                
+                <p>Kelola Lab SMK Negeri 4 Malang</p>
+                <label for="username">Username</label>
+                <input class="username" type="text" placeholder="Ex: johndepp12">
+                <label for="username">Password</label>
+                <input class="password" type="password" placeholder="Min. 8 character">
+                <div class="checkbox-text">
+                    <div class="checkbox-content">
+                    <input type="checkbox" value="" id="flexCheckDefault">
+                    <label for="flexCheckDefault">
+                        Ingat Saya
+                    </label>
+
+                    </div>
+                    <a href="">Lupa Password?</a>
+                </div>
+                
+                <button>Masuk</button>
+            </form>
+        </div>
+        <div class="right">
+                <img src="images/login.png" alt="">
+            </div>
+    </div>
+</body>
